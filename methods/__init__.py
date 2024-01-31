5061e4807c4af58f9266dbeb68bf2cdc46b22fed
Paul-Tru <99089100+Paul-Tru@users.noreply.github.com>
2024-01-31 20:27:51 +0100

    get info about your pc (eg. vram)

--- methods/__init__.py
+++ methods/__init__.py
@@ -2,3 +2,4 @@ from .search_devices import search_devices
 from .check_os import check_os
 # from .stt import whisper_stt
 from .check_network import check_network
+from .get_sys_info import get_sys_info
---
+++ methods/get_sys_info.py
@@ -0,0 +1,5 @@
+import os
+import logging
+
+def get_sys_info():
+    pass
